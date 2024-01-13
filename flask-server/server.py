a179eac93d899e20a5030abd4aa660d0b68736d1
Ryland-Seagraves <rseagrav@vols.utk.edu>
2024-01-13 13:58:00 -0500

    devcontainer fixes

--- flask-server/server.py
+++ flask-server/server.py
@@ -5,11 +5,7 @@ app = Flask(__name__)
 #for testing if Flask is online
 @app.route('/', methods=['GET'])
 def status_200():
-  return jsonify({'message': 'Hi Bear!'}), 200
-
-@app.route('/login', methods=['GET'])
-def status_200():
-  return 200
+  return jsonify({'messages': ['Hi Bear!', 'Hi Katelyn!']}), 200
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(debug=True, host='0.0.0.0', port=5000)
