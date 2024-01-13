b345fb93f2000d80633ad9fc03997738826d6b2f
Ryland-Seagraves <rseagrav@vols.utk.edu>
2024-01-13 10:56:08 -0500

    devcontainers

--- flask-server/server.py
+++ flask-server/server.py
@@ -2,15 +2,14 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-
 #for testing if Flask is online
 @app.route('/', methods=['GET'])
 def status_200():
-    return jsonify({'message': 'OK'}), 200
+  return jsonify({'message': 'Hi Bear!'}), 200
 
 @app.route('/login', methods=['GET'])
-def status_501():
-    return 501
+def status_200():
+  return 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
