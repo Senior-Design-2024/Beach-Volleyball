8cc42d47d392c3b4448f2588b517a4ffd1f5b954
Ryland-Seagraves <rseagrav@vols.utk.edu>
2024-01-20 17:10:22 -0500

    Backend setup for frontend testing

--- flask-server/server.py
+++ flask-server/server.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def status_200():
   return jsonify({'messages': ['Hi Bear!', 'Hi Katelyn!']}), 200
 
+@app.route('/test')
+def test():
+  return {'message': 'working'}
+
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=5000)
