9f6e2bedf68a039b891c1484a3a2a92c3292ea6e
Bear Moran <ejmoran5@gmail.com>
2024-03-13 06:11:09 -0400

    create login form

--- react-frontend/src/pages/Home.js
+++ react-frontend/src/pages/Home.js
@@ -8,7 +8,21 @@ export default function Home() {
   return (
     <div id='page-wrapper'>
       <AppHeader/>
-      <h1>app home page</h1>
+      <h1>About us</h1>
+      <p>
+          Beach volleyball stats app is a senior design project being built by Bear Moran and Ryland Seagraves at the request of Buzz Patrick.
+        We are seniors at the University of Tennessee Knoxville.
+        We entered this project with little experience with the tools we have used and the skills to design and create a web app, but we are learning a lot!
+        <br/>
+          This app is under heavy development. If you are seeing this page, you are most likely part of the development or testing team, know them personally, or are a bot scrapping the internet.
+        In the latter case, we have nothing of interest for you so it is best you move along.
+        If you are viewing this page on the internet, a special thanks goes out to Ryland for getting AWS up and running.
+        <br/>
+          {/*Feel free to explore. The following image may be helpful for navigating the aqua buttons.*/}
+      </p>
+      {/*
+      <img src='../images/bvsa_flowchart.png' alt='bvsa_flowchart'></img>
+  */}
     </div>
   );
 }
