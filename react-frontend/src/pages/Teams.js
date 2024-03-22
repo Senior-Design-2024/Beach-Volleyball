1a616cd40d6c7a4ed9d464d8fa562280aa511e76
Bear Moran <ejmoran5@gmail.com>
2024-03-22 18:45:42 -0400

    conditional render teams

--- react-frontend/src/pages/Teams.js
+++ react-frontend/src/pages/Teams.js
@@ -1,22 +1,57 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { BasicButton } from '../components/basic_components'
 import AppHeader from '../components/AppHeader';
 
 export default function Teams() {
+  const [teams, setTeams] = useState({
+    names: ['test1', 'test2'],
+  });
+
+  //navigate functions
   const navigate = useNavigate();
   const navigateHome = () => navigate('/');
   const navigateAddTeam = () => navigate('/AddTeam');
   const navigatePlayers = () => navigate('/Players');
 
   //html
+  let display_teams;
+
+  //displays teams if there is at least one
+  if(teams.names.length === 0) {
+    display_teams = (
+      <div id='dev-wrapper'>
+        <p>Please add a team</p>
+        <p>*dev button to progress to next page*</p>
+        <BasicButton onClick={navigatePlayers} buttonText='select teams (to players)'></BasicButton>
+      </div>
+    );
+  } else {
+    display_teams = (
+      <div>
+        {teams.names.map(( (name) => (
+          <div id='button-wrapper'>
+            <br/>
+            <BasicButton onClick={navigatePlayers} buttonText={name}/>
+          </div>
+        )))}
+        <p>
+          *dev notes*<br/>
+          these buttons have no functionality other than moving to the next page
+        </p>
+      </div>
+    );
+  }
+
   return (
-    <div id='page-wrapper' className='wrapper'>
+    <div id='page-wrapper' className='page-wrapper'>
       <AppHeader masthead={'Welcome!'}
         leftButtonNames={['Create Team']} 
         leftButtonFunctions={[navigateAddTeam]}
         rightButtonNames={['Logout']}
-        rightButtonFunctions={[navigateHome]}/>
-      <BasicButton onClick={navigatePlayers} buttonText='select teams (to players)'></BasicButton>
+        rightButtonFunctions={[navigateHome]}
+      />
+      {display_teams}
     </div>
   );
 }
