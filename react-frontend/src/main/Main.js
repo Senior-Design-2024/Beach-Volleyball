80def4bd4a6c05332136932d6d2ec67a3e784d11
Bear Moran <ejmoran5@gmail.com>
2024-04-06 14:12:28 -0400

    conditionally render pages working

--- react-frontend/src/main/Main.js
+++ react-frontend/src/main/Main.js
@@ -1,25 +1,29 @@
+import { useState } from 'react';
 import '../App.css'
+import AppHeader from '../components/AppHeader';
+import Home from './Home';
+import NewUser from './NewUser';
+import Login from './Login';
 
 export default function Main() {
+  const [currentView, setCurrentView] = useState('home')
+
+  const dispHome = () => {setCurrentView('home')}
+  const dispCreateAccount = () => {setCurrentView('newuser')}
+  const dispLogin = () => {setCurrentView('login')}
+  
+  //html
   return (
     <div id='main-page-wrapper' className='page-wrapper'>
       <AppHeader masthead={'Beach Volleyball Stats App'}
-        leftButtonNames={['']}
-        leftButtonFunctions={[navigateHome, navigateNewUser]}
-        rightButtonNames={['']}
-        rightButtonFunctions={[navigateLogin]}/>
+        leftButtonNames={['Home', 'Create Account']}
+        leftButtonFunctions={[dispHome, dispCreateAccount]}
+        rightButtonNames={['Login']}
+        rightButtonFunctions={[dispLogin]}/>
 
-      <h1>About us</h1>
-      <p>
-          Beach volleyball stats app is a senior design project being built by Bear Moran and Ryland Seagraves at the request of Buzz Patrick.
-        We are seniors at the University of Tennessee Knoxville.
-        We entered this project with little experience with the tools we have used and the skills to design and create a web app, but we are learning a lot!
-        <br/>
-          This app is under heavy development. If you are seeing this page, you are most likely part of the development or testing team, know them personally, or are a bot scraping the internet.
-        In the latter case, we have nothing of interest for you, so it is best you move along.
-        If you are viewing this page on the internet, a special thanks goes out to Ryland for getting AWS up and running.
-        <br/>
-      </p>
+      {currentView === 'home' && <Home/>}
+      {currentView === 'newuser' && <NewUser/>}
+      {currentView === 'login' && <Login/>}
     </div>
   );
 }
---
+++ react-frontend/src/main/NewUser.js
@@ -0,0 +1,7 @@
+export default function NewUser() {
+  return(
+    <div id='new-user'>
+      
+    </div>
+  )
+}
