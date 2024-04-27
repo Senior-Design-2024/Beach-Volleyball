ccc80eb0180e6eadc6dc7c9e0b34c188ea3d316b
BuildTools <unconfigured@null.spigotmc.org>
2024-04-27 19:38:51 -0400

    delete paths, dbstruct with cascading deletes, modification paths

--- flask-server/dbstruct.sql
+++ flask-server/dbstruct.sql
@@ -11,7 +11,7 @@ CREATE TABLE `team`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL,
     `user_id` BIGINT UNSIGNED NOT NULL,
-    CONSTRAINT `team_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `user`(`id`)
+    CONSTRAINT `team_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
 );
 ALTER TABLE
     `team` ADD INDEX `team_user_id_index`(`user_id`);
@@ -20,16 +20,16 @@ CREATE TABLE `player`(
     `team_id` BIGINT UNSIGNED NOT NULL,
     `name` VARCHAR(255) NOT NULL,
     `description` TEXT NOT NULL,
-    CONSTRAINT `player_team_id_foreign` FOREIGN KEY(`team_id`) REFERENCES `team`(`id`)
+    CONSTRAINT `player_team_id_foreign` FOREIGN KEY(`team_id`) REFERENCES `team`(`id`) ON DELETE CASCADE
 );
 CREATE TABLE `pair` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `team_id` BIGINT UNSIGNED NOT NULL,
     `player1_id` BIGINT UNSIGNED NOT NULL,
     `player2_id` BIGINT UNSIGNED NOT NULL,
-    CONSTRAINT `pair_team_id_foreign` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`),
-    CONSTRAINT `pair_player1_foreign` FOREIGN KEY (`player1_id`) REFERENCES `player` (`id`),
-    CONSTRAINT `pair_player2_foreign` FOREIGN KEY (`player2_id`) REFERENCES `player` (`id`)
+    CONSTRAINT `pair_team_id_foreign` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`) ON DELETE CASCADE,
+    CONSTRAINT `pair_player1_foreign` FOREIGN KEY (`player1_id`) REFERENCES `player` (`id`) ON DELETE CASCADE,
+    CONSTRAINT `pair_player2_foreign` FOREIGN KEY (`player2_id`) REFERENCES `player` (`id`) ON DELETE CASCADE
 );
 CREATE TABLE `match`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -52,26 +52,26 @@ CREATE TABLE `match`(
     `strategy` VARCHAR(100),
     CONSTRAINT `match_player2_foreign` FOREIGN KEY(`player2_id`) REFERENCES `player`(`id`),
     CONSTRAINT `match_player1_foreign` FOREIGN KEY(`player1_id`) REFERENCES `player`(`id`),
-    CONSTRAINT `match_team_id_foreign` FOREIGN KEY(`team_id`) REFERENCES `team`(`id`),
-    CONSTRAINT `match_pair_id_foreign` FOREIGN KEY(`pair_id`) REFERENCES `pair`(`id`)
+    CONSTRAINT `match_team_id_foreign` FOREIGN KEY(`team_id`) REFERENCES `team`(`id`) ON DELETE CASCADE,
+    CONSTRAINT `match_pair_id_foreign` FOREIGN KEY(`pair_id`) REFERENCES `pair`(`id`) ON DELETE CASCADE
 );
 CREATE TABLE `match_set`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `match_id` BIGINT UNSIGNED NOT NULL,
     `set_num` TINYINT UNSIGNED NOT NULL,
     `win_state` BOOLEAN,
-    CONSTRAINT `match_set_match_id_foreign` FOREIGN KEY(`match_id`) REFERENCES `match`(`id`)
+    CONSTRAINT `match_set_match_id_foreign` FOREIGN KEY(`match_id`) REFERENCES `match`(`id`) ON DELETE CASCADE
 );
 CREATE TABLE `point`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `match_set_id` BIGINT UNSIGNED NOT NULL,
     `win` BOOLEAN NOT NULL,
-    CONSTRAINT `point_match_set_id_foreign` FOREIGN KEY (`match_set_id`) REFERENCES `match_set`(`id`)
+    CONSTRAINT `point_match_set_id_foreign` FOREIGN KEY (`match_set_id`) REFERENCES `match_set`(`id`) ON DELETE CASCADE
 );
 CREATE TABLE `event`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `point_id` BIGINT UNSIGNED NOT NULL,
     `data` INT UNSIGNED NOT NULL,
     `e_index` TINYINT UNSIGNED NOT NULL,
-    CONSTRAINT `event_point_id_foreign` FOREIGN KEY (`point_id`) REFERENCES `point`(`id`)
+    CONSTRAINT `event_point_id_foreign` FOREIGN KEY (`point_id`) REFERENCES `point`(`id`) ON DELETE CASCADE
 );
--- flask-server/mod_path.py
+++ flask-server/mod_path.py
@@ -8,48 +8,84 @@ def update_entity(entity_type, id):
     entity = None
 
     if entity_type == 'user':
+
         entity = db.session.get(User, id)
+
         if entity:
             entity.username = data.get('username', entity.username)
             entity.email = data.get('email', entity.email)
+
     elif entity_type == 'team':
+
         entity = db.session.get(Team, id)
-        if entity:
+
+        user = User.query.filter_by(email=data.get('email')).first()
+
+        if entity and user:
             entity.name = data.get('team_name', entity.name)
-            entity.email = data.get('email', entity.email)
+            entity.user = user
+
     elif entity_type == 'player':
+
         entity = db.session.get(Player, id)
-        if entity:
+        team = db.session.get(Team, data.get('team_id'))
+
+        if entity and team:
+
             entity.name = data.get('player_name', entity.name)
             entity.description = data.get('description', entity.description)
-    elif entity_type == 'match':
-        entity = db.session.get(Match, id)
-        if entity:
-            team = db.session.get(Team, entity.team_id)
-            player1 = db.session.get(Player, entity.player1_id)
-            player2 = db.session.get(Player, entity.player2_id)
-            pair = db.session.get(Pair, entity. pair_id)
+            entity.team = team
 
-            if pair and team and player1 and player2:
-                entity.team = team
+    elif entity_type == 'match':
+
+        entity = db.session.get(Match, id)
+
+        team = db.session.get(Team, entity.team_id)
+        player1 = db.session.get(Player, entity.player1_id)
+        player2 = db.session.get(Player, entity.player2_id)
+        pair = db.session.get(Pair, entity. pair_id)
+
+        if entity and pair and team and player1 and player2:
+
+            entity.team = team
+            entity.player1 = player1
+            entity.player2 = player2
+            entity.pair = pair
+            entity.opponent1_name = data.get('opponent1_name') #Can be empty ''
+            entity.opponent2_name = data.get('opponent2_name') #Can be empty ''
+            entity.opponent1_number = data.get('opponent1_number') #Can be empty ''
+            entity.opponent2_number = data.get('opponent2_number') #Can be empty ''
+            entity.venue = data.get('venue') #Can be empty ''
+            entity.tournament = data.get('tournament') #Can be empty ''
+            entity.court_number = data.get('court_number') #Can be empty ''
+            entity.flight_number = data.get('flight_number') #Can be empty ''
+            entity.conference = data.get('conference') #Can be 0, 1, or 2
+            entity.location = data.get('location') #Can be 0, 1, 2, or 3
+            entity.match_date = data.get('match_date') #Must be date format, can be NULL for undated
+            entity.sched_start_time = data.get('sched_start_time') #Must be time format, can be NULL for no time
+            entity.strategy = data.get('strategy') #Can be 0, 1, 2, or 3
+
+    elif entity_type == 'pair':
+        entity = db.session.get(Pair, id)
+
+        player1 = db.session.get(Player, entity.player1_id)
+        player2 = db.session.get(Player, entity.player2_id)
+        team = db.session.get(Team, entity.team_id)
+
+        if entity and player1 and player2 and team:
                 entity.player1 = player1
                 entity.player2 = player2
-                entity.pair = pair
-                entity.opponent1_name = data.get('opponent1_name') #Can be empty ''
-                entity.opponent2_name = data.get('opponent2_name') #Can be empty ''
-                entity.opponent1_number = data.get('opponent1_number') #Can be empty ''
-                entity.opponent2_number = data.get('opponent2_number') #Can be empty ''
-                entity.venue = data.get('venue') #Can be empty ''
-                entity.tournament = data.get('tournament') #Can be empty ''
-                entity.court_number = data.get('court_number') #Can be empty ''
-                entity.flight_number = data.get('flight_number') #Can be empty ''
-                entity.conference = data.get('conference') #Can be 0, 1, or 2
-                entity.location = data.get('location') #Can be 0, 1, 2, or 3
-                entity.match_date = data.get('match_date') #Must be date format, can be NULL for undated
-                entity.sched_start_time = data.get('sched_start_time') #Must be time format, can be NULL for no time
-                entity.strategy = data.get('strategy') #Can be 0, 1, 2, or 3
+                entity.team = team
+    
     elif entity_type == 'set':
         entity = db.session.get(MatchSet, id)
+
+        match = db.session.get(Match, match_id)
+
+        if entity and match:
+            entity.match = match
+            entity.win_state = data.get('win_state')
+            entity.set_num = data.get('set_num')
     
 
     if entity:
