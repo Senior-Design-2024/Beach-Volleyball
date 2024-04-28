bb7ee02cdab87eb8ddaf8d52be1a4f6a29be96d8
Bear Moran <ejmoran5@gmail.com>
2024-04-27 23:18:33 -0400

    players path added

--- react-frontend/src/user/Teams.js
+++ react-frontend/src/user/Teams.js
@@ -2,17 +2,28 @@ import React, { useContext } from 'react';
 import { UserContext } from './User';
 
 export default function Teams(props) { 
-  const {teams} = useContext(UserContext)
+  const {teams, setTeamData} = useContext(UserContext)
+
+  const selectTeam = (team_id, team_name) => {
+    console.log(team_id, team_name)
+    setTeamData(prevState => ({
+      ...prevState,
+      team_id: team_id,
+      team_name: team_name,
+    }))
+
+    props.dispPlayers()
+  }
   
   return(
     <div id='teams'>
       <button onClick={props.dispNewTeam}>New Team</button>
-      
+
       <div id='list-teams'>
         {teams.map(( (team) => (
           <div key={team.id} id='button-wrapper'>
             <br/>
-            <button>{team.name}</button>
+            <button onClick={() => selectTeam(team.id, team.name)}>{team.name}</button>
           </div>
         )))}
       </div>
--- react-frontend/src/user/User.js
+++ react-frontend/src/user/User.js
@@ -6,6 +6,7 @@ import Teams from './Teams';
 import NewTeam from './NewTeam';
 import { useLocation } from 'react-router-dom';
 import { findRequest } from '../utils';
+import Players from './Players';
 
 export const UserContext = createContext();
 
@@ -17,7 +18,7 @@ export default function User() {
   const [teams, setTeams] = useState([])
   const [teamData, setTeamData] = useState({
     team_name: '',
-    team_id: '',
+    team_id: -1,
     players: [],
   })
 
@@ -36,6 +37,26 @@ export default function User() {
     getTeams()
   }, [user_id]);
 
+  useEffect(() => {
+    const getPlayers = () => {
+      findRequest('player', 'team_id', teamData.team_id).then(
+        (players) => {
+          setTeamData(prevState => ({
+            ...prevState,
+            players: players,
+          }));
+        }).catch(
+          (error) => {
+            console.error('Error', error);
+          }
+      );
+    };
+    console.log('slkj')
+    if (teamData.team_id !== -1) {
+      getPlayers();
+    }
+  }, [teamData.team_id]); 
+  
 
   //display
   const [currentView, setCurrentView] = useState('teams')
@@ -44,6 +65,9 @@ export default function User() {
   }
   const dispNewTeam = () => {
     setCurrentView('newTeam')
+  }
+  const dispPlayers = () => {
+    setCurrentView('players')
   }
   
   const navigate = useNavigate()
@@ -59,8 +83,9 @@ export default function User() {
       
       {/*children*/}
       <UserContext.Provider value={{user_id, teams, setTeams, getTeams, teamData, setTeamData}}>
-        {currentView === 'teams' && <Teams dispNewTeam={dispNewTeam}/>}
+        {currentView === 'teams' && <Teams dispNewTeam={dispNewTeam} dispPlayers={dispPlayers}/>}
         {currentView === 'newTeam' && <NewTeam dispTeams={dispTeams}/>}
+        {currentView === 'players' && <Players/>}
       </UserContext.Provider>
     </div>
   );
