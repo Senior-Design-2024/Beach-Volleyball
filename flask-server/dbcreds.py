0f6849666b1a51cfc491342d7bb7586f095feb16
Ryland-Seagraves <112524465+Ryland-Seagraves@users.noreply.github.com>
2024-05-03 20:42:14 -0400

    Update dbcreds.py

--- flask-server/dbcreds.py
+++ flask-server/dbcreds.py
@@ -1,4 +1,4 @@
-dbhost = 'sdgn-2.ct8yq608ajrv.us-east-2.rds.amazonaws.com:3306'
-dbuser = 'root'
-dbpass = ':ubY(^5&|cevY8U28JSR'
-dbname = 'flask_app'
+dbhost = 'HIDDEN'
+dbuser = 'HIDDEN'
+dbpass = 'HIDDEN'
+dbname = 'HIDDEN'
